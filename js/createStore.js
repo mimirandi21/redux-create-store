9d6c7c7d5a87c83387fde9ab8031bc0dc06f5462
Randi Schreiner <mimirandi21@gmail.com>
2021-02-17 13:27:42 -0600

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,28 +1,35 @@
-let state;
+function createStore(reducer) {
+	let state;
+
+	function dispatch(action) {
+		state = reducer(state, action);
+		render();
+	}
+
+	function getState() {
+		return state;
+	}
+	return { dispatch, getState };
+}
 
 function reducer(state = { count: 0 }, action) {
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return { count: state.count + 1 };
+	switch (action.type) {
+		case "INCREASE_COUNT":
+			return { count: state.count + 1 };
 
-    default:
-      return state;
-  }
-};
-
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
+		default:
+			return state;
+	}
+}
 
 function render() {
-  let container = document.getElementById('container');
-  container.textContent = state.count;
-};
+	let container = document.getElementById("container");
+	container.textContent = store.getState().count;
+}
+let store = createStore(reducer);
+store.dispatch({ type: "@@INIT" });
+let button = document.getElementById("button");
 
-dispatch({ type: '@@INIT' })
-let button = document.getElementById('button');
-
-button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
-})
+button.addEventListener("click", function () {
+	store.dispatch({ type: "INCREASE_COUNT" });
+});
